2d072f4430daf145dac0d8f81de3be4014022bd0
Nikita Marchant <nikita.marchant@gmail.com>
2016-11-14 14:18:33 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,22 @@ use nom::IResult::{Done, Error, Incomplete};
 mod parsers;
 
 
+fn eval(expression: parsers::LispVal) -> parsers::LispVal {
+    expression
+}
+
+fn format_value(value: parsers::LispVal) -> String {
+    match value {
+        parsers::LispVal::Bool(val) => format!("Bool {:?}", val),
+        parsers::LispVal::Atom(val) => format!("Atom: {:?}", val),
+        parsers::LispVal::Number(val) => format!("Number: {:?}", val),
+        parsers::LispVal::String(val) => format!("String {:?}", val),
+        parsers::LispVal::List(val) => format!("List {:?}", val),
+        _ => format!("Unknown value")
+    }
+}
+
+
 fn main() {
     let mut reader = rustyline::Editor::<()>::new();
 
@@ -22,17 +38,11 @@ fn main() {
                     continue;
                 }
                 match parsers::command(line.trim().as_bytes()) {
-                    Done(_, matched) => {
-                        match matched {
-                            parsers::LispVal::Bool(val) => println!("Bool {:?}", val),
-                            parsers::LispVal::Atom(val) => println!("Atom: {:?}", val),
-                            parsers::LispVal::Number(val) => println!("Number: {:?}", val),
-                            parsers::LispVal::String(val) => println!("String {:?}", val),
-                            parsers::LispVal::List(val) => println!("List {:?}", val),
-                            _ => println!("WAT ?")
-                        }
+                    Done(_, expression) => {
+                        let result = eval(expression);
+                        println!("{}", format_value(result));
                     },
-                    Error(_) | Incomplete(_) => println!("error"),
+                    Error(_) | Incomplete(_) => println!("SyntaxError"),
                 }
             }
             Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => {
